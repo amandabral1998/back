37f37d02f161636298e556d17da086f20ae145b2
Aman Dabral <125656656+amandabral1998@users.noreply.github.com>
2023-10-27 15:36:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors  = require('cors')
 const mongoose = require('mongoose')
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: '*', 
     credentials: true, // Enable credentials (cookies) in requests
   }));
 
