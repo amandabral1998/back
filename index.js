edcc75cc113e58051eefc31aded0910cd0fd1e04
Aman Dabral <125656656+amandabral1998@users.noreply.github.com>
2023-12-14 21:05:21 +0530

    Update index.js
    Modified CORS Origin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors  = require('cors')
 const mongoose = require('mongoose')
 
 app.use(cors({
-    origin: 'https://ecommerceplatform2.netlify.app', 
+    origin: 'https://ecommerceplatform1.netlify.app', 
     credentials: true, // Enable credentials (cookies) in requests
   }));
 
